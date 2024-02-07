094b8512356f7046d757f39913fd7fe6e63e37dd
SanteCalderisi112358 <santecalderisi@gmail.com>
2024-02-07 16:51:14 +0100

    image-random

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-s
+const imageContainerEl = document.querySelector('.image-container')
+const btnEl = document.querySelector('.btn')
+const imageNumber = 10
+btnEl.addEventListener('click', () => {
+    
+    addNewImages()
+})
+
+function addNewImages() {
+    for (let index = 0; index < imageNumber; index++) {
+        const newImgEl = document.createElement('img')
+        newImgEl.src = `https://picsum.photos/300?random=${Math.floor(Math.random() * 2000)}`
+        imageContainerEl.appendChild(newImgEl)
+
+    }
+
+}
